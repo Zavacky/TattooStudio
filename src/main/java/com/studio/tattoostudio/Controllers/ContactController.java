14e1c3e809211d99ad7e0a28c42d7b119e224d6c
Tomas Olsavsky <tomas.olsavsky2@gmail.com>
2023-12-12 20:13:32 +0100

    ContactScene + zaciatok menu + Registration (confirm pass)

---
+++ src/main/java/com/studio/tattoostudio/Controllers/ContactController.java
@@ -0,0 +1,30 @@
+package com.studio.tattoostudio.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class ContactController {
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private ComboBox<?> purposeComboBox;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField surnameTextField;
+
+    @FXML
+    private TextField telephoneTextField;
+
+}
--- src/main/java/com/studio/tattoostudio/Controllers/RegistrationController.java
+++ src/main/java/com/studio/tattoostudio/Controllers/RegistrationController.java
@@ -8,24 +8,26 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
 public class RegistrationController {
-    public class PleaseProvideControllerClassName {
 
-        @FXML
-        private PasswordField passwordTextfield;
+    @FXML
+    private PasswordField passwordTextField;
 
-        @FXML
-        private Button registerButton;
+    @FXML
+    private Button registerButton;
 
-        @FXML
-        private ImageView logoImageview;
+    @FXML
+    private ImageView logoImageview;
 
-        @FXML
-        private TextField loginTextField;
+    @FXML
+    private PasswordField confirmpasswordTextfield;
 
-        @FXML
-        void onRegister(ActionEvent event) {
+    @FXML
+    private TextField loginTextField;
 
-        }
+    @FXML
+    void onRegister(ActionEvent event) {
+
+    }
 
 
     }
