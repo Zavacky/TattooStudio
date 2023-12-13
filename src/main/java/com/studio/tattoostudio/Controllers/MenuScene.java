fc560714999d6e7daa4dca73fd6487c052739040
Tomas Olsavsky <tomas.olsavsky2@gmail.com>
2023-12-13 01:40:34 +0100

    Menu + TatterProfileScene

---
+++ src/main/java/com/studio/tattoostudio/Controllers/MenuScene.java
@@ -0,0 +1,36 @@
+package com.studio.tattoostudio.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Pagination;
+
+public class MenuScene {
+
+    @FXML
+    private Button peterButton;
+
+    @FXML
+    private Pagination aboutusphotosPagination;
+
+    @FXML
+    private Button timeaButton;
+
+    @FXML
+    private Button feroButton;
+
+    @FXML
+    void onClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClick(ActionEvent event) {
+
+    }
+}
