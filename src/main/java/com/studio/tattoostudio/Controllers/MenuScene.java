7977425c5c5ff56ed6da6d76b16ef1287ff1dad5
Tomas Olsavsky <tomas.olsavsky2@gmail.com>
2023-12-14 09:55:22 +0100

    Galleria navrhov  + Salony scenes

--- src/main/java/com/studio/tattoostudio/Controllers/MenuScene.java
+++ src/main/java/com/studio/tattoostudio/Controllers/MenuScene.java
@@ -2,14 +2,28 @@ package com.studio.tattoostudio.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Pagination;
 
-public class MenuScene {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    private Pagination oldphotosPagination;
+public class MenuScene implements Initializable{
+        @FXML
+        private Pagination oldphotosPagination;
 
+        @FXML
+        private ChoiceBox<String> whatcityChoiceBox;
+
+        public void ini
+
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+                whatcityChoiceBox.getItems().addAll("Kosice", "Bardejov", "Humenne");
+        }
 }
+
 
 
